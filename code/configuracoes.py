0b62bd5410452f4bc587af698aa5273cb7e1fd10
Pedro Ailton <pedroailton630@gmail.com>
2025-07-13 11:59:44 -0300

    Dados dos personagens

---
+++ code/configuracoes.py
@@ -0,0 +1,17 @@
+PERSONAGENS = {
+    'Espadachim': {'poder': 'espada', 'vida': 200},
+    'Arqueiro': {'poder': 'arco', 'vida': 140},
+    'Mago': {'poder': 'magia', 'vida': 100},
+    'Lacaio': {'poder': 'garras', 'vida': 100},
+    'O Desertor': {'poder': 'magia', 'vida': 1000},
+}
+
+HABILIDADES = {
+    'golpe de espada': {'dano': 50, 'poder': 'espada', 'animation': 'golpe_de_espada'},
+    'golpe com as mãos': {'dano': 20, 'poder': 'espada', 'animation': 'golpe_com_maos'},
+    'atirar flecha': {'dano': 40, 'poder': 'arco', 'animation': 'arco_e_flecha'},
+    'lançar magia': {'dano': 45, 'poder': 'espada', 'animation': 'golpe_com_maos'},
+    'arranhar': {'dano': 25, 'poder': 'garras', 'animation': 'arranhar'},
+    'meteoro': {'dano': 70, 'poder': 'magia', 'animation': 'meteoro'},
+    'lançar raio': {'dano': 100, 'poder': 'magia', 'animation': 'raio'},
+}
